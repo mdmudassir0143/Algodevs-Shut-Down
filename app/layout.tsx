68dc3395729248c73c704cfbe3208d7dcd9c310c
Mohammad Mudassir <mohammadmudassir@Mohammads-MacBook-Air.local>
2025-12-16 13:53:53 +0530

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,21 +13,6 @@ export const metadata: Metadata = {
     "Thank you for being part of the Algodevs journey. Track your contributions on the Algorand Global Leaderboard.",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
   },
 }
 
